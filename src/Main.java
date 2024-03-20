be9b768f6398054e68a8e2f5f12c4e16ec94fffb
GaspardGentil <gaspard.gentil@etu.univ-lyon1.fr>
2024-03-20 14:05:35 +0100

    base code and subject push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+
+import VueControleur.VueControleur;
+import modele.Jeu;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Jeu jeu = new Jeu();
+
+        VueControleur vc = new VueControleur(jeu);
+        vc.setVisible(true);
+
     }
 }
