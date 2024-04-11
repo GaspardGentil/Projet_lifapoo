58b909c45fe1e7b4bdd11b6d4cfb166e461c26f2
Gaspard Gentil <gaspard.gentil@etu.univ-lyon1.com>
2024-04-11 14:32:36 +0200

    Fix, Add Fix : typo in Goal class Add : UML changes to match the new code

--- src/modele/Goal.java
+++ src/modele/Goal.java
@@ -4,8 +4,8 @@ public class Goal extends Case  {
 
     private Heros hero;
     private Bloc b;
-    public Goal(Jeu _jeu) { super(_jeu); }
 
+    public Goal(Jeu _jeu) { super(_jeu); }
     public boolean peutEtreParcouru() { return e == null; }
 
 }
